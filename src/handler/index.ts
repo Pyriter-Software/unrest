377da866d92b7f26bf9cd1039ada210d612aa6d4
Phong Vuong <pyriter.io@gmail.com>
2023-12-20 06:32:42 -0800

    Adding references to index

--- src/handler/index.ts
+++ src/handler/index.ts
@@ -1,3 +1,6 @@
-export * from './getHandler';
+export * from './typeHandler/deleteHandler';
+export * from './typeHandler/getHandler';
+export * from './typeHandler/postHandler';
+export * from './typeHandler/putHandler';
 export * from './handler';
 export * from './requestPath';
